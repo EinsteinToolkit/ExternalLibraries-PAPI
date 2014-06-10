f6bf9d6effbbd02877e6508c4439948c10b324e5
eschnett <eschnett@40d618a3-1497-40ce-b2b1-9261a9dddc7a>
2014-06-10 22:26:48 +0000

    Update PAPI to 5.3.0
    git-svn-id: http://svn.cactuscode.org/projects/ExternalLibraries/PAPI/trunk@13 40d618a3-1497-40ce-b2b1-9261a9dddc7a

--- configure.sh
+++ configure.sh
@@ -83,6 +83,8 @@ then
     THORN=PAPI
     NAME=papi-5.2.0
     TARNAME=papi-5.2.0
+    #NAME=papi-5.3.0
+    #TARNAME=papi-5.3.0
     SRCDIR=$(dirname $0)
     BUILD_DIR=${SCRATCH_BUILD}/build/${THORN}
     if [ -z "${PAPI_INSTALL_DIR}" ]; then
@@ -131,7 +133,7 @@ then
         echo "PAPI: Unpacking archive..."
         pushd ${BUILD_DIR}
         ${TAR?} xzf ${SRCDIR}/dist/${TARNAME}.tar.gz
-
+        
         echo "PAPI: Applying patches..."
         pushd ${NAME}
         ${PATCH?} -p1 < ${SRCDIR}/dist/remove_Werror.patch
@@ -152,6 +154,24 @@ then
         
         echo "PAPI: Configuring..."
         cd ${NAME}/src
+        # CC          C compiler command
+        # CFLAGS      C compiler flags
+        # LDFLAGS     linker flags, e.g. -L<lib dir> if you have libraries in a
+        #             nonstandard directory <lib dir>
+        # CPPFLAGS    C/C++ preprocessor flags, e.g. -I<include dir> if you have
+        #             headers in a nonstandard directory <include dir>
+        # F77         Fortran 77 compiler command
+        # FFLAGS      Fortran 77 compiler flags
+        # CPP         C preprocessor
+        #unset CC
+        #unset CFLAGS
+        #unset LDFLAGS
+        #unset CPPFLAGS
+        #unset F77
+        #unset FFLAGS
+        #unset CPP
+        #export MPICC=
+
         ./configure --prefix=${PAPI_DIR} --with-shared-lib=no
         
         echo "PAPI: Building..."
