9b3332b6f217280bf9fea303810e41cdfcbfa4b7
eschnett <eschnett@40d618a3-1497-40ce-b2b1-9261a9dddc7a>
2014-06-12 16:46:53 +0000

    Define Cactus clocks for avg, min, max as well
    git-svn-id: http://svn.cactuscode.org/projects/ExternalLibraries/PAPI/trunk@14 40d618a3-1497-40ce-b2b1-9261a9dddc7a

--- src/clock.c
+++ src/clock.c
@@ -3,6 +3,8 @@
 #include <cctk_Parameters.h>
 
 #include <assert.h>
+#include <float.h>
+#include <limits.h>
 #include <math.h>
 #include <stdarg.h>
 #include <stdbool.h>
@@ -30,10 +32,13 @@ static int num_clock_vals = -1;
 
 static const int counter_nsec  = 0;
 static const int counter_cyc   = 1;
-static const int counter_begin = 2;
+static const int counter_count = 2;
+static const int counter_begin = 3;
 
-static const char **counter_names = NULL; // [num_event_vals]
-static const char **counter_units = NULL; // [num_event_vals]
+static int num_statistics_vals   = -1;
+static int num_cactus_clock_vals = -1;
+static const char **counter_names = NULL; // [num_cactus_clock_vals]
+static const char **counter_units = NULL; // [num_cactus_clock_vals]
 
 
 
@@ -43,6 +48,7 @@ typedef struct {
   // indexing: [num_clock_vals], laid out as [num_threads][num_event_vals]
   papi_counter_t *restrict accum;    // [num_clock_vals]
   papi_counter_t *restrict snapshot; // [num_clock_vals]
+  papi_counter_t count;
   bool running;
 } papi_clock_t;
 
@@ -68,6 +74,7 @@ static void read_papi_counters(papi_counter_t *restrict const counters)
                         &my_counters[counter_begin], &my_counters[counter_cyc]);
     // chkerr(ierr, "PAPI_read_ts");
     if (ierr<0) my_counters[counter_cyc] = PAPI_get_real_cyc();
+    ++my_counters[counter_count];
   }
   /* for (int i=0; i<num_clock_vals; ++i) { */
   /*   assert(counters[i] != -1); */
@@ -140,21 +147,72 @@ static void clock_get(const int timernum,
                       void *restrict const data_,
                       cTimerVal *restrict const vals)
 {
+  DECLARE_CCTK_PARAMETERS;
+  
   const papi_clock_t *restrict const data = data_;
   
+  int count = 0;
   for (int i=0; i<num_event_vals; ++i) {
-    papi_counter_t sum = 0;
+    
+    double sum = 0.0, sum2 = 0.0, min = DBL_MAX, max = 0.0;
     for (int j=i; j<num_clock_vals; j+=num_event_vals) {
       /* assert(data->accum[j] >= 0); */
-      sum += data->accum[j];
+      const double val = data->accum[j];
+      sum += val;
+      sum2 += val*val;
+      min = fmin(min, val);
+      max = fmax(max, val);
     }
-    /* assert(sum >= 0); */
-    vals[i].type       = val_double;
-    vals[i].heading    = counter_names[i];
-    vals[i].units      = counter_units[i];
-    vals[i].val.d      = sum / 1.0e+9;
-    vals[i].seconds    = sum / 1.0e+9;
-    vals[i].resolution = 1.0e-9;
+    /* assert(sum >= 0.0); */
+    
+    if (!define_statistics_clocks) {
+      
+      vals[count].type       = val_double;
+      vals[count].heading    = counter_names[count];
+      vals[count].units      = counter_units[count];
+      vals[count].val.d      = sum / 1.0e+9;
+      vals[count].seconds    = sum / 1.0e+9;
+      vals[count].resolution = 1.0e-9;
+      ++count;
+      
+    } else {
+      
+      const double avg = sum / num_threads;
+      const double sdv = sqrt(fmax(0.0, sum2 / num_threads - avg*avg));
+      
+      vals[count].type       = val_double;
+      vals[count].heading    = counter_names[count];
+      vals[count].units      = counter_units[count];
+      vals[count].val.d      = avg / 1.0e+9;
+      vals[count].seconds    = avg / 1.0e+9;
+      vals[count].resolution = 1.0e-9;
+      ++count;
+      
+      vals[count].type       = val_double;
+      vals[count].heading    = counter_names[count];
+      vals[count].units      = counter_units[count];
+      vals[count].val.d      = sdv / 1.0e+9;
+      vals[count].seconds    = sdv / 1.0e+9;
+      vals[count].resolution = 1.0e-9;
+      ++count;
+      
+      vals[count].type       = val_double;
+      vals[count].heading    = counter_names[count];
+      vals[count].units      = counter_units[count];
+      vals[count].val.d      = min / 1.0e+9;
+      vals[count].seconds    = min / 1.0e+9;
+      vals[count].resolution = 1.0e-9;
+      ++count;
+      
+      vals[count].type       = val_double;
+      vals[count].heading    = counter_names[count];
+      vals[count].units      = counter_units[count];
+      vals[count].val.d      = max / 1.0e+9;
+      vals[count].seconds    = max / 1.0e+9;
+      vals[count].resolution = 1.0e-9;
+      ++count;
+      
+    }
   }
 }
 
@@ -175,46 +233,79 @@ static void clock_set(const int timernum,
 
 
 
+static void add_clock(const char** names, const char** units,
+                      const char* name, const char* unit,
+                      int* clock, int nclocks)
+{
+  DECLARE_CCTK_PARAMETERS;
+  if (!define_statistics_clocks) {
+    assert(*clock < nclocks);
+    names[*clock] = strdup(name);
+    units[*clock] = unit;
+    ++*clock;
+  } else {
+    const char* const suffixes[] = {"_avg", "_sdv", "_min", "_max"};
+    for (int s=0; s<4; ++s) {
+      assert(*clock < nclocks);
+      char buf[1000];
+      snprintf(buf, sizeof buf, "%s%s", name, suffixes[s]);
+      names[*clock] = strdup(buf);
+      units[*clock] = unit;
+      ++*clock;
+    }
+  }
+}
+
 void PAPI_register_clock(CCTK_ARGUMENTS)
 {
   DECLARE_CCTK_ARGUMENTS;
+  DECLARE_CCTK_PARAMETERS;
   
   CCTK_INFO("PAPI_register_clock");
   
   int ierr;
   
-  // Clock names
-  
   num_event_vals = counter_begin + num_events;
-  counter_names = malloc(num_event_vals * sizeof *counter_names);
+  
+  // PAPI clocks
+  
+  num_clock_vals = num_threads * num_event_vals;
+  
+  // Cactus clocks
+  
+  num_statistics_vals = define_statistics_clocks ? 4 : 1;
+  num_cactus_clock_vals = num_event_vals * num_statistics_vals;
+  
+  counter_names = malloc(num_cactus_clock_vals * sizeof *counter_names);
   assert(counter_names);
-  counter_units = malloc(num_event_vals * sizeof *counter_units);
+  counter_units = malloc(num_cactus_clock_vals * sizeof *counter_units);
   assert(counter_units);
-  counter_names[counter_nsec] = "P_time";
-  counter_units[counter_nsec] = "sec";
-  counter_names[counter_cyc] = "P_cycles";
-  counter_units[counter_cyc] = "Gcycles";
+  
+  int clock = 0;
+  add_clock(counter_names, counter_units, "P_time", "sec",
+            &clock, num_cactus_clock_vals);
+  add_clock(counter_names, counter_units, "P_cycles", "Gcycles",
+            &clock, num_cactus_clock_vals);
+  add_clock(counter_names, counter_units, "P_count", "Gevents",
+            &clock, num_cactus_clock_vals);
   for (int i=0; i<num_events; ++i) {
-    char eventname[PAPI_MAX_STR_LEN];
+    char eventname[PAPI_MAX_STR_LEN+4];
     ierr = PAPI_event_code_to_name(events[i], eventname);
     chkerr(ierr, "PAPI_event_code_to_name");
     if (strlen(eventname)>=4) {
       // Change prefix "PAPI" to "P" to save space
       for (char *p=eventname+1; p[2]; ++p) p[0] = p[3];
     }
-    counter_names[counter_begin+i] = strdup(eventname);
-    counter_units[counter_begin+i] = "Gevents";
+    add_clock(counter_names, counter_units, eventname, "Gevents",
+              &clock, num_cactus_clock_vals);
   }
-  
-  // Combined
-  
-  num_clock_vals = num_threads * num_event_vals;
+  assert(clock == num_cactus_clock_vals);
   
   
   
   cClockFuncs funcs;
   funcs.name    = clock_name;
-  funcs.n_vals  = num_event_vals;
+  funcs.n_vals  = num_cactus_clock_vals;
   funcs.create  = clock_create;
   funcs.destroy = clock_destroy;
   funcs.start   = clock_start;
