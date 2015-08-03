01a292bdd6c9e1242fc305d738c017adbb016f08
eschnett <eschnett@40d618a3-1497-40ce-b2b1-9261a9dddc7a>
2015-08-03 19:40:19 +0000

    Apply clang-format
    git-svn-id: http://svn.cactuscode.org/projects/ExternalLibraries/PAPI/trunk@30 40d618a3-1497-40ce-b2b1-9261a9dddc7a

--- src/stats.h
+++ src/stats.h
@@ -7,12 +7,10 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-
-
 #define num_threads PAPI_Cactus_num_threads
-#define eventsets   PAPI_Cactus_eventsets
-#define num_events  PAPI_Cactus_num_events
-#define events      PAPI_Cactus_events
+#define eventsets PAPI_Cactus_eventsets
+#define num_events PAPI_Cactus_num_events
+#define events PAPI_Cactus_events
 
 // Number of threads
 extern int num_threads;
@@ -22,9 +20,7 @@ extern int *restrict eventsets; // [num_threads]
 // Number of events in these eventsets
 extern int num_events;
 // Events in these eventsets
-extern int *restrict events;    // [num_events]
-
-
+extern int *restrict events; // [num_events]
 
 // Output a a debug message
 #define outinfo PAPI_Cactus_outinfo
@@ -33,8 +29,6 @@ void outinfo(const char *const function);
 // Check for an error, and output an error message if there was one
 #define chkerr PAPI_Cactus_chkerr
 void chkerr(const int ierr, const char *const function, ...)
-  CCTK_ATTRIBUTE_FORMAT(printf, 2, 3);
+    CCTK_ATTRIBUTE_FORMAT(printf, 2, 3);
 
-
-
-#endif  // #ifndef STATS_H
+#endif // #ifndef STATS_H
